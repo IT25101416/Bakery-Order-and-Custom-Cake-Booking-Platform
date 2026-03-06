c37f2f7dfaf9e9ca7a79eeb76857591ed4c7492f
Ishodya Nimnadi <ishodyanimnadi5519@gmail.com>
2026-03-06 14:33:07 +0530

    Added UI templates and text files for data storage

---
+++ src/test/java/com/example/bakeryproject/BakeryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bakeryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeryProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
