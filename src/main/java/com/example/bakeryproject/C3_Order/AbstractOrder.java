e3361f9830efd185658965aa5e0224f383262fc8
IT25102052 <it25102052@my.sliit.lk>
2026-05-01 20:44:58 +0530

    Abstract base class. Provides Abstraction — defines what all orders must do without saying how

--- src/main/java/com/example/bakeryproject/C3_Order/AbstractOrder.java
+++ src/main/java/com/example/bakeryproject/C3_Order/AbstractOrder.java
@@ -2,6 +2,8 @@ package com.example.bakeryproject.C3_Order;
 
 public abstract class AbstractOrder {
 
+    //Forces all order types to implement these methods
+
     public abstract double calculateTotal();
     public abstract String getOrderSummary();
     public abstract String getOrderType();
