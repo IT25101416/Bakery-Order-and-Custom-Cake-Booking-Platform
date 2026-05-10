b5cd003882914bd4960a53e6eecfe122ada78a4f
IT25102052 <it25102052@my.sliit.lk>
2026-05-10 18:38:18 +0530

    update  Order class

--- src/main/java/com/example/bakeryproject/C3_Order/Order.java
+++ src/main/java/com/example/bakeryproject/C3_Order/Order.java
@@ -60,4 +60,4 @@ public class Order extends AbstractOrder {
     public String getStatus()                      { return status; }
     public void   setStatus(String status)         { this.status = status; }
 }
-}
+
