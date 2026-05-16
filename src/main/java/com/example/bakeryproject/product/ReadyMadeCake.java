5ff5c015d1d9e098e81339df4c092544e55d9aaa
IT25102377 <it25102377@my.sliit.lk>
2026-05-16 20:51:53 +0530

    create sub class for cake. create ReadyMadeCake.java

--- src/main/java/com/example/bakeryproject/product/ReadyMadeCake.java
+++ src/main/java/com/example/bakeryproject/product/ReadyMadeCake.java
@@ -1,4 +1,24 @@
 package com.example.bakeryproject.product;
 
-public class ReadyMadeCake {
+
+//inheritance
+public class ReadyMadeCake extends Cake {
+
+    //default constructor
+    public ReadyMadeCake() {
+    }
+
+    //parameterize constructor
+    public ReadyMadeCake(String cakeId, String cakeName, String category,
+                         double price, int quantity) {
+        super(cakeId, cakeName, category, price, quantity);
+    }
+
+    //overriding
+    @Override
+    public double calculatePrice() {
+        return getPrice();
+    }
 }
+
+
