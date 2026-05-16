332d190905f879b03c14c9d3aac08fcc00bfba05
IT25102377 <it25102377@my.sliit.lk>
2026-05-16 18:33:37 +0530

    use encapcsulation

--- src/main/java/com/example/bakeryproject/product/Cake.java
+++ src/main/java/com/example/bakeryproject/product/Cake.java
@@ -1,4 +1,9 @@
 package com.example.bakeryproject.product;
 
 public class Cake {
-}
+    private String cakeId;
+    private String cakeName;
+    private String category;
+    private double price;
+    private int quantity;
+
