f9be8ec0e05b771047335c3800032544acc558da
IT25102052 <it25102052@my.sliit.lk>
2026-05-17 11:07:26 +0530

    Add order processor interface

--- src/main/java/com/example/bakeryproject/order/OrderProcessor.java
+++ src/main/java/com/example/bakeryproject/order/OrderProcessor.java
@@ -1,4 +1,5 @@
 package com.example.bakeryproject.order;
 
-public class OrderProcessor {
+public abstract class OrderProcessor {
+    public abstract double calculateTotal(double price, int quantity);
 }
