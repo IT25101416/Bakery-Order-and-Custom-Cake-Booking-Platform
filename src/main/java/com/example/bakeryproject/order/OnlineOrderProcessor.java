d653fa84dca3355b13cbbbc764e83478228a1004
IT25102052 <it25102052@my.sliit.lk>
2026-05-17 11:13:37 +0530

    Implement online order processor

--- src/main/java/com/example/bakeryproject/order/OnlineOrderProcessor.java
+++ src/main/java/com/example/bakeryproject/order/OnlineOrderProcessor.java
@@ -1,4 +1,8 @@
 package com.example.bakeryproject.order;
 
-public class OnlineOrderProcessor {
+public class OnlineOrderProcessor extends OrderProcessor{
+    @Override
+    public double calculateTotal(double price, int quantity) {
+        return price * quantity;
+    }
 }
