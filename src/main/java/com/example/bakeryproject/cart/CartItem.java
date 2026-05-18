da310580218cea0217c5907de1ec90ee69140006
IT25103078 <it25103078@my.sliit.lk>
2026-05-18 11:13:02 +0530

    create CartItem model implementing Serializable with subtotal calculation

--- src/main/java/com/example/bakeryproject/cart/CartItem.java
+++ src/main/java/com/example/bakeryproject/cart/CartItem.java
@@ -1,4 +1,68 @@
 package com.example.bakeryproject.cart;
 
-public class CartItem {
+
+import java.io.Serializable;
+
+public class CartItem implements Serializable {
+    private String cakeId;
+    private String cakeName;
+    private String category;
+    private double price;
+    private int quantity;
+
+    public CartItem() {
+    }
+
+    public CartItem(String cakeId, String cakeName, String category, double
+            price, int quantity) {
+        this.cakeId = cakeId;
+        this.cakeName = cakeName;
+        this.category = category;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public double getSubTotal() {
+        return price * quantity;
+    }
+
+    public String getCakeId() {
+        return cakeId;
+    }
+
+    public void setCakeId(String cakeId) {
+        this.cakeId = cakeId;
+    }
+
+    public String getCakeName() {
+        return cakeName;
+    }
+
+    public void setCakeName(String cakeName) {
+        this.cakeName = cakeName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
