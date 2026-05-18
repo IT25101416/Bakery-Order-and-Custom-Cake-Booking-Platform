3f1650d71a038c05df2c5254cf1de02ecedd0d0d
MalinduDulaj <malindudulaj16@gmail.com>
2026-05-18 23:57:45 +0530

    Add order processor interface

--- src/main/java/com/example/bakeryproject/order/OrderProcessor.java
+++ src/main/java/com/example/bakeryproject/order/OrderProcessor.java
@@ -1,6 +1,10 @@
 package com.example.bakeryproject.order;
 
-
+/**
+ * Abstract class that defines the contract for order processing.
+ * Any class that processes orders must extend this class
+ * and provide an implementation for calculateTotal().
+ */
 public abstract class OrderProcessor {
     public abstract double calculateTotal(double price, int quantity);
 }
