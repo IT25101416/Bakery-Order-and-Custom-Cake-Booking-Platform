70f2e983d76cb55f9ec152c33166438bad725e45
IT25103078 <it25103078@my.sliit.lk>
2026-05-18 10:38:04 +0530

    Add permissionLevel and file reading/writing methods to Admin class

--- src/main/java/com/example/bakeryproject/admin/Admin.java
+++ src/main/java/com/example/bakeryproject/admin/Admin.java
@@ -1,4 +1,36 @@
 package com.example.bakeryproject.admin;
 
-public class Admin {
+import com.example.bakeryproject.common.User;
+
+public class Admin extends User {
+    private String permissionLevel;
+
+    public Admin() {
+    }
+
+    public Admin(String id, String name, String email, String password,
+                 String permissionLevel) {
+        super(id, name, email, password);
+        this.permissionLevel = permissionLevel;
+    }
+
+    public String toFileString() {
+        return getId() + "," + getName() + "," + getEmail() + "," +
+                getPassword() + "," + permissionLevel;
+    }
+
+    public static Admin fromFileString(String line) {
+        String[] data = line.split(",");
+        return new Admin(data[0], data[1], data[2], data[3], data[4]);
+    }
+
+    public String getPermissionLevel() {
+        return permissionLevel;
+    }
+
+    public void setPermissionLevel(String permissionLevel) {
+        this.permissionLevel = permissionLevel;
+    }
 }
+
+
